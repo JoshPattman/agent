a6c2e3f3eb19646d8e028c8fdf73550445c3e23a
josh.pattman <josh.pattman@luminance.com>
2025-10-09 18:58:25 +0100

    Add 2 more types of tools

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/JoshPattman/agent
 
 go 1.23.1
 
-require github.com/JoshPattman/jpf v0.8.1
+require (
+	github.com/JoshPattman/jpf v0.8.1
+	github.com/mitchellh/mapstructure v1.5.0
+)
--- tools.go
+++ tools.go
@@ -4,7 +4,74 @@ import (
 	"bytes"
 	"encoding/json"
 	"slices"
+
+	"github.com/JoshPattman/jpf"
+	"github.com/mitchellh/mapstructure"
 )
+
+func MapFuncTool[T any](mf jpf.MapFunc[T, string], name string, description []string) Tool {
+	return &mapFuncTool[T]{
+		mf:   mf,
+		name: name,
+		desc: description,
+	}
+}
+
+type mapFuncTool[T any] struct {
+	mf   jpf.MapFunc[T, string]
+	name string
+	desc []string
+}
+
+// Call implements Tool.
+func (m *mapFuncTool[T]) Call(args map[string]any) (string, error) {
+	var typedArgs T
+	err := mapstructure.Decode(args, &typedArgs)
+	if err != nil {
+		return "", err
+	}
+	result, _, err := m.mf.Call(typedArgs)
+	return result, err
+}
+
+// Description implements Tool.
+func (m *mapFuncTool[T]) Description() []string {
+	return slices.Clone(m.desc)
+}
+
+// Name implements Tool.
+func (m *mapFuncTool[T]) Name() string {
+	return m.name
+}
+
+func FunctionalTool(do func(map[string]any) (string, error), name string, description []string) Tool {
+	return &functionalTool{
+		do:   do,
+		name: name,
+		desc: description,
+	}
+}
+
+type functionalTool struct {
+	do   func(map[string]any) (string, error)
+	name string
+	desc []string
+}
+
+// Call implements Tool.
+func (f *functionalTool) Call(args map[string]any) (string, error) {
+	return f.do(args)
+}
+
+// Description implements Tool.
+func (f *functionalTool) Description() []string {
+	return slices.Clone(f.desc)
+}
+
+// Name implements Tool.
+func (f *functionalTool) Name() string {
+	return f.name
+}
 
 func AgentAsTool(agent *Agent, name string, description []string) Tool {
 	return &agentAsTool{agent, name, description}
